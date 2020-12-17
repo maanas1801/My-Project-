d0618060465ded88a3547d23d7a03847bfb4ba34
maanas1801 <73119612+maanas1801@users.noreply.github.com>
2020-12-17 18:03:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SIKAR | Home</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<header>
+    <nav class="navbar">
+    	<div id="logo">
+	    <h1>sikar</h1>
+        </div>
+    	<div class="menu">
+    		<a href="index.php">HOME</a>
+    		<a href="services.php">SERVICES</a>
+    		<a href="projects.php">PROJECTS</a>
+    		<a href="contact.php">CONTACT</a>
+    	
+    	</div>
+    </nav>
+
+    <main>
+    	<div>
+    	<h1>welcome</h1>
+    	</div>
+    </header>
+
+    <section>
+    	<div class="us">
+    	<h3>ABOUT <span class="high">US</span></h3>
+         </div>
+         <div>
+    	<P>
+    	SIKAR Construction has been attracting, holding and moulding the finest engineering talent in India and abroad. People are the Company's most valued asset - its core strength. Sikar also has India's largest fleet of sophisticated and specialized equipment for speedy, high-quality and cost-effective construction.
+
+Fully mechanized stone and metal quarries and crushing plants throughout India strengthen the resource base of the Company; their operations are completely environment friendly which further sustains SIKAR's growth.
+<br><br>GLOBAL SUPPLY CHAIN<br><br>
+The ISO certified Materials Management function of SIKAR, the most important link in Supply Chain Management, supports the global operations of the Company by catering to the needs of all its offices, factories and projects spread across India and abroad.
+<br><br>SAFETY & QUALITY<br><br>
+At SIKAR, the highest priority is accorded to Environment, Health & Safety (EHS). The EHS policy enunciated by the Corporate Management lays emphasis on Environment, Health and Safety through a structured approach and well defined practices. Systems and procedures have been established for implementing the requisites at all stages of construction and are accredited to International standards of ISO 9001:2008, ISO 14001:2004 and OHSAS 18001:2007.
+
+SIKAR continues to maintain its lofty tradition of meeting the stringent quality standards and adherence to time schedules across all projects. SIKAR has established a robust ISO certified Quality Management Systems for all its core and support functions. Many feats of SIKAR in meeting the stringent quality requirements of its projects and products won national and international recognitions.
+</P>
+    </div>
+    </section>
+    </main>
+
+</body>
+ 
+ <footer>
+ 	 <p>SIKAR Companies &copy; 2021</p>
+ </footer>
+</html>
